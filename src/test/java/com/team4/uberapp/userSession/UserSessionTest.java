1c2697ef4991f714341b6235a150bb0eefaec55e
echolinr <echo.zhailin@gmail.com>
2016-11-25 01:11:22 +0800

    milstone 4 - session testing
    1) add new test case: canNotPostWithInvalidToken,
    canNotPostRidesBeforeLoginWithoutToken,
    canNotPostCarsInDriverBeforeLoginWithoutToken,canNotLoginWithWrongUser,canPostCarAfterLogin
    2) verify returned token having userID

--- src/test/java/com/team4/uberapp/userSession/UserSessionTest.java
+++ src/test/java/com/team4/uberapp/userSession/UserSessionTest.java
@@ -3,6 +3,7 @@ package com.team4.uberapp.userSession;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team4.uberapp.MongoConfiguration;
 import com.team4.uberapp.UberAppMain;
+import com.team4.uberapp.car.Car;
 import com.team4.uberapp.domain.Repositories;
 import com.team4.uberapp.passenger.Passenger;
 import com.team4.uberapp.persistence.MongoRepositories;
@@ -17,9 +18,7 @@ import java.util.UUID;
 
 import static com.team4.uberapp.util.UberAppUtil.hashPassword;
 import static com.team4.uberapp.util.UberAppUtil.validTokenUser;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static spark.Spark.awaitInitialization;
 import static spark.Spark.stop;
 
@@ -71,8 +70,76 @@ public class UserSessionTest {
         }
 
     }
+
     @Test
-    public void canLogInWithValidPassengerEmailPassword () {
+    public void canNotPostRidesBeforeLoginWithoutToken () {
+        String reqestJson = "{" +
+                "\"make\" : \"isuzu\","+
+                "\"model\" : \"sx4\"," +
+                "\"license\" : \"88PX123\"," +
+                "\"carType\" : \"sedan\"," +
+                "\"color\": \"blue\"," +
+                "\"maxPassengers\" : 5,"+
+                "\"validRideTypes\" : \"ECONOMY\"}";
+        try {
+            //make a post
+            SparkTestUtil.UrlResponse response = http.doMethod("POST", "/v1/rides", reqestJson, "application/json");
+            assertEquals(401,response.status);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            assertTrue(false);
+        }
+
+    }
+
+    @Test
+    public void canNotPostCarsInDriverBeforeLoginWithoutToken () {
+        String reqestJson = "{" +
+                "\"make\" : \"isuzu\","+
+                "\"model\" : \"sx4\"," +
+                "\"license\" : \"88PX123\"," +
+                "\"carType\" : \"sedan\"," +
+                "\"color\": \"blue\"," +
+                "\"maxPassengers\" : 5,"+
+                "\"validRideTypes\" : \"ECONOMY\"}";
+        try {
+            //make a post
+            SparkTestUtil.UrlResponse response = http.doMethod("POST", "/v1/drivers/", reqestJson, "application/json");
+            assertEquals(401,response.status);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            assertTrue(false);
+        }
+
+    }
+
+    @Test
+    public void canNotPostWithInvalidToken () {
+        //post car without token
+        String path = path ="/v1/cars?token=adfadfajbhoiadsjfadfa";
+        String reqestJson = "{" +
+                "\"make\" : \"isuzu\","+
+                "\"model\" : \"sx4\"," +
+                "\"license\" : \"88PX123\"," +
+                "\"carType\" : \"sedan\"," +
+                "\"color\": \"blue\"," +
+                "\"maxPassengers\" : 5,"+
+                "\"validRideTypes\" : \"ECONOMY\"}";
+
+        try {
+            //make a post
+            SparkTestUtil.UrlResponse response = http.doMethod("POST", path, reqestJson, "application/json");
+            assertEquals(401,response.status);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            assertTrue(false);
+        }
+
+    }
+
+
+    @Test
+    public void canLogInTokenContainUserId () {
         // add a passenger in to db
         Passenger passenger = new Passenger("Test", "Unit", "tunit@west.abc.com", "12345678", "Sillon Valley", null, "Sunnyvale", "CA",
                                    "94089", "123-456-7890");
@@ -112,6 +179,110 @@ public class UserSessionTest {
             System.out.println(e.getMessage());
             assertTrue(false);
         }
+    }
+    @Test
+    public void canNotLoginWithWrongUser () {
+        // add a passenger in to db
+        Passenger passenger = new Passenger("Test", "Unit", "tunit@west.abc.com", "12345678", "Sillon Valley", null, "Sunnyvale", "CA",
+                "94089", "123-456-7890");
+        passenger.setId(UUID.randomUUID());
+        MongoSession session = MongoConfiguration.createSession();
+        session.start();
+        Repositories.initialise(new MongoRepositories(session));
+        passenger.setPassword(hashPassword(passenger.getPassword()));
+        Repositories.passengers().add(passenger);
+        session.stop();
 
+        // using GET/v1/car/:id
+        String path = "/v1/sessions";
+        String reqestJson = "{" +
+                "\"email\" : \"tunit@abc.com\","+
+                "\"password\" : \"12345678\"}";
+        try {
+            //make a post
+            SparkTestUtil.UrlResponse response = http.doMethod("POST", path, reqestJson, "application/json");
+
+            // remove testing data
+            session = MongoConfiguration.createSession();
+            session.start();
+            Repositories.initialise(new MongoRepositories(session));
+            Repositories.passengers().get(passenger.getId());
+            Repositories.passengers().delete(passenger);
+            session.stop();
+
+            //verify value
+            assertEquals(401, response.status);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            assertTrue(false);
+        }
+    }
+    @Test
+    public void canPostCarAfterLogin () {
+        // add a passenger in to db
+        Passenger passenger = new Passenger("Test", "Unit", "tunit@west.abc.com", "12345678", "Sillon Valley", null, "Sunnyvale", "CA",
+                "94089", "123-456-7890");
+        passenger.setId(UUID.randomUUID());
+        MongoSession session = MongoConfiguration.createSession();
+        session.start();
+        Repositories.initialise(new MongoRepositories(session));
+        passenger.setPassword(hashPassword(passenger.getPassword()));
+        Repositories.passengers().add(passenger);
+        session.stop();
+
+        // using GET/v1/car/:id
+        String path = "/v1/sessions";
+        String reqestJson = "{" +
+                "\"email\" : \"tunit@west.abc.com\","+
+                "\"password\" : \"12345678\"}";
+        try {
+            //make a post
+            SparkTestUtil.UrlResponse response = http.doMethod("POST", path, reqestJson, "application/json");
+            //get result of the post
+            ObjectMapper mapper = new ObjectMapper();
+            SessionToken returnToken = mapper.readValue(response.body, SessionToken.class);
+
+            //post a car
+            path ="/v1/cars?token=" + returnToken.getToken();
+            reqestJson = "{" +
+                    "\"make\" : \"isuzu\","+
+                    "\"model\" : \"sx4\"," +
+                    "\"license\" : \"88PX123\"," +
+                    "\"carType\" : \"sedan\"," +
+                    "\"color\": \"blue\"," +
+                    "\"maxPassengers\" : 5,"+
+                    "\"validRideTypes\" : \"ECONOMY\"}";
+
+            //make a post
+            response = http.doMethod("POST", path, reqestJson, "application/json");
+            //get result of the post
+            Car testCar = mapper.readValue(response.body, Car.class);
+
+            // check db
+            session = MongoConfiguration.createSession();
+            session.start();
+            Repositories.initialise(new MongoRepositories(session));
+            Car dbCar =Repositories .cars().get(testCar.getId());
+
+            //removing testing data
+            Repositories.cars().delete(dbCar);
+            passenger = Repositories.passengers().get(passenger.getId());
+            Repositories.passengers().delete(passenger);
+            session.stop();
+
+            //verify value
+            assertEquals(200, response.status);
+            assertEquals(testCar.getId(),dbCar.getId());
+            assertEquals(testCar.getMake(), dbCar.getMake());
+            assertEquals(testCar.getModel(), dbCar.getModel());
+            assertEquals(testCar.getLicense(),dbCar.getLicense());
+            assertEquals(testCar.getCarType(), dbCar.getCarType());
+            assertEquals(testCar.getMaxPassengers(), dbCar.getMaxPassengers());
+            assertEquals(testCar.getColor(), dbCar.getColor());
+            assertEquals(testCar.getValidRideTypes(),dbCar.getValidRideTypes());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            assertTrue(false);
+        }
     }
 }
